729afc3a58571e11506b54bbe387452889d54c27
John Regan <john@ubuntu-12-docker.(none)>
2013-12-17 08:34:28 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+docker-tinynode
+=============
+
+A small [buildroot](http://buildroot.uclibc.org/)-based image for [Docker](http://docker.io/), with [NodeJS](http://nodejs.org/) installed.
+
+The following useful modules/frameworks are installed:
+
+* [Express](http://expressjs.com/) 
+* [CoffeeScript](http://coffeescript.org/)
+* [Socket IO](http://socket.io/)
+* [Underscore.js](http://underscorejs.org/)
+* [forever](https://github.com/nodejitsu/forever)
+
+Currently using buildroot 2013.11 and NodeJS 0.8.22
---
+++ tinynode_build.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+set -e
+
+imagename=tinynode
+
+cdir=$(pwd)
+source ./${imagename}_config
+
+buildroot_dir=${cdir}/buildroot-${buildroot_version}
+docker_dir=${cdir}/Docker
+
+if [[ ! -d ${buildroot_dir} ]]
+then
+  curl ${buildroot_url} | tar jx
+  cp ./${imagename}_defconfig ${buildroot_dir}/configs/${imagename}_defconfig
+  cd ${buildroot_dir} && make ${imagename}_defconfig && make
+  cd ${cdir}
+else
+  # Probably just doing a rebuild
+  cp ./${imagename}_defconfig ${buildroot_dir}/configs/${imagename}_defconfig
+  cd ${buildroot_dir}
+  make ${imagename}_defconfig && make
+  for package in "${packages[@]}"
+  do
+      echo "building ${package}"
+      make ${package}-rebuild
+  done
+  cd ${cdir}
+fi
+
+mv ${buildroot_dir}/output/images/rootfs.tar ${docker_dir}
+
